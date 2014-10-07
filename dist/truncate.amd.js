d053c36c95ad0730f76f007e6ce2481a12b2eee0
Tim Evans <tim-evans@sproutcore.com>
2014-10-06 21:19:13 -0400

    modularize line splitting so we can split on an outer block's tokens

--- dist/truncate.amd.js
+++ dist/truncate.amd.js
@@ -1,4 +1,51 @@
-define("truncate/string", 
+define("truncate", 
+  ["truncate/string","truncate/lines","dom-ruler/text","dom-ruler/layout","exports"],
+  function(__dependency1__, __dependency2__, __dependency3__, __dependency4__, __exports__) {
+    "use strict";
+    var split = __dependency1__.split;
+    var trim = __dependency1__.trim;
+    var words = __dependency2__.words;
+    var measure = __dependency3__.measure;
+    var layoutOf = __dependency4__.layout;
+
+    var truncate = function (fragment, options) {
+      options.ellipsis = options.ellipsis || '&hellip;';
+      options.block = options.block || fragment;
+      options.lineBreak = options.lineBreak || 'normal';
+
+      var width = layoutOf(fragment).width;
+      options.width = width;
+      options.template = fragment;
+
+      var metrics = words(fragment.innerHTML, options);
+      var lines = metrics.lines;
+
+      // Compute the size of the ellipsis
+      var ellipsisWidth = measure(options.ellipsis, { template: fragment, escape: false }).width;
+
+      if (lines.length > options.lines) {
+        var noTokens = 0;
+        var line;
+        for (var i = 0, len = options.lines; i < len; i++) {
+          line = lines[i];
+          noTokens += line.length;
+        }
+
+        var tokens = metrics.words.slice(0, noTokens);
+        var lastToken = line[line.length - 1];
+        while (lastToken.left + lastToken.width + ellipsisWidth > width) {
+          tokens.pop();
+          line.pop();
+          lastToken = line[line.length - 1];
+        }
+        fragment.innerHTML = trim(tokens.join('')) + options.ellipsis;
+      }
+    };
+
+    var truncate = truncate;
+    __exports__.truncate = truncate;
+  });
+;define("truncate/string", 
   ["exports"],
   function(__exports__) {
     "use strict";
@@ -61,31 +108,26 @@ define("truncate/string",
     __exports__.split = split;
     __exports__.trim = trim;
   });
-;define("truncate/truncate", 
+;define("truncate/lines", 
   ["truncate/string","dom-ruler/text","dom-ruler/layout","exports"],
   function(__dependency1__, __dependency2__, __dependency3__, __exports__) {
     "use strict";
     var split = __dependency1__.split;
-    var trim = __dependency1__.trim;
     var prepareTextMeasurement = __dependency2__.prepareTextMeasurement;
     var measureText = __dependency2__.measureText;
     var teardownTextMeasurement = __dependency2__.teardownTextMeasurement;
-    var measure = __dependency2__.measure;
     var layoutOf = __dependency3__.layout;
 
-    var truncate = function (fragment, options) {
-      options.ellipsis = options.ellipsis || '&hellip;';
-
+    function words (string, options) {
       // Split the text where the line may break
-      var text = split(fragment.innerHTML, { lineBreak: 'strict', lang: options.lang });
+      var text = split(string, { lineBreak: options.lineBreak, lang: options.lang });
       var html = [];
       for (var i = 0, len = text.length; i < len; i++) {
         html[i] = '<span>' + text[i] + '</span>';
       }
 
       // Prepare the test element
-      var width = layoutOf(fragment).width;
-      var element = prepareTextMeasurement(fragment, { width: width + 'px' });
+      var element = prepareTextMeasurement(options.template, { width: options.width + 'px' });
 
       // Compute the location of each <span>, resulting in line metrics
       measureText(html.join(''), false);
@@ -123,27 +165,11 @@ define("truncate/string",
       // Teardown the test element
       teardownTextMeasurement();
 
-      // Compute the size of the ellipsis
-      var ellipsisWidth = measure(options.ellipsis, { template: fragment, escape: false }).width;
-
-      if (lines.length > options.lines) {
-        var noTokens = 0;
-        for (i = 0, len = options.lines; i < len; i++) {
-          line = lines[i];
-          noTokens += line.length;
-        }
-
-        var tokens = text.slice(0, noTokens);
-        var lastToken = line[line.length - 1];
-        while (lastToken.left + lastToken.width + ellipsisWidth > width) {
-          tokens.pop();
-          line.pop();
-          lastToken = line[line.length - 1];
-        }
-        fragment.innerHTML = trim(tokens.join('')) + options.ellipsis;
-      }
+      return {
+        words: text,
+        lines: lines
+      };
     };
 
-    var truncate = truncate;
-    __exports__.truncate = truncate;
+    __exports__.words = words;
   });
