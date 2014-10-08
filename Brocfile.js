986dba7543f5cac520f840a33531711e5c03b619
Tim Evans <tim.evans@paddle8.com>
2014-10-08 11:05:11 -0400

    rename truncate to truncate-text

--- Brocfile.js
+++ Brocfile.js
@@ -12,7 +12,7 @@ var lib = compileES6(mergeTrees(['lib', 'bower_components/loader.js']), {
     '**/*.js'
   ],
   wrapInEval: false,
-  outputFile: '/truncate.js'
+  outputFile: '/truncate-text.js'
 });
 
 var amd = compileES6('lib', {
@@ -21,7 +21,7 @@ var amd = compileES6('lib', {
   ],
   ignoredModules: ['dom-ruler/layout', 'dom-ruler/text', 'dom-ruler/utils'],
   wrapInEval: false,
-  outputFile: '/truncate.amd.js'
+  outputFile: '/truncate-text.amd.js'
 });
 
 var vendorAmd = pickFiles('bower_components/dom-ruler/dist', {
@@ -38,13 +38,13 @@ var vendor = pickFiles('bower_components/dom-ruler/dist', {
 
 var standalone = concat(mergeTrees([amd, vendor]), {
   inputFiles: ['**/*.js'],
-  outputFile: '/truncate.all.js',
+  outputFile: '/truncate-text.all.js',
   wrapInEval: false
 });
 
 var standaloneAmd = concat(mergeTrees([amd, vendorAmd]), {
   inputFiles: ['**/*.js'],
-  outputFile: '/truncate.all.amd.js',
+  outputFile: '/truncate-text.all.amd.js',
   wrapInEval: false
 });
 
@@ -60,11 +60,11 @@ var uglify = function (tree, filename) {
 
 module.exports = mergeTrees([
   lib,
-  uglify(lib, 'truncate.js'),
+  uglify(lib, 'truncate-text.js'),
   standalone,
-  uglify(standalone, 'truncate.all.js'),
+  uglify(standalone, 'truncate-text.all.js'),
   amd,
-  uglify(amd, 'truncate.amd.js'),
+  uglify(amd, 'truncate-text.amd.js'),
   standaloneAmd,
-  uglify(standaloneAmd, 'truncate.all.amd.js')
+  uglify(standaloneAmd, 'truncate-text.all.amd.js')
 ]);
--- dist/truncate-text.all.amd.js
+++ dist/truncate-text.all.amd.js
@@ -1,150 +1,4 @@
-var define, requireModule, require, requirejs;
-
-(function() {
-
-  var _isArray;
-  if (!Array.isArray) {
-    _isArray = function (x) {
-      return Object.prototype.toString.call(x) === "[object Array]";
-    };
-  } else {
-    _isArray = Array.isArray;
-  }
-
-  var registry = {}, seen = {};
-  var FAILED = false;
-
-  var uuid = 0;
-
-  function tryFinally(tryable, finalizer) {
-    try {
-      return tryable();
-    } finally {
-      finalizer();
-    }
-  }
-
-
-  function Module(name, deps, callback, exports) {
-    var defaultDeps = ['require', 'exports', 'module'];
-
-    this.id       = uuid++;
-    this.name     = name;
-    this.deps     = !deps.length && callback.length ? defaultDeps : deps;
-    this.exports  = exports || { };
-    this.callback = callback;
-    this.state    = undefined;
-  }
-
-  define = function(name, deps, callback) {
-    if (!_isArray(deps)) {
-      callback = deps;
-      deps     =  [];
-    }
-
-    registry[name] = new Module(name, deps, callback);
-  };
-
-  define.amd = {};
-
-  function reify(mod, name, seen) {
-    var deps = mod.deps;
-    var length = deps.length;
-    var reified = new Array(length);
-    var dep;
-    // TODO: new Module
-    // TODO: seen refactor
-    var module = { };
-
-    for (var i = 0, l = length; i < l; i++) {
-      dep = deps[i];
-      if (dep === 'exports') {
-        module.exports = reified[i] = seen;
-      } else if (dep === 'require') {
-        reified[i] = function requireDep(dep) {
-          return require(resolve(dep, name));
-        };
-      } else if (dep === 'module') {
-        mod.exports = seen;
-        module = reified[i] = mod;
-      } else {
-        reified[i] = require(resolve(dep, name));
-      }
-    }
-
-    return {
-      deps: reified,
-      module: module
-    };
-  }
-
-  requirejs = require = requireModule = function(name) {
-    var mod = registry[name];
-    if (!mod) {
-      throw new Error('Could not find module ' + name);
-    }
-
-    if (mod.state !== FAILED &&
-        seen.hasOwnProperty(name)) {
-      return seen[name];
-    }
-
-    var reified;
-    var module;
-    var loaded = false;
-
-    seen[name] = { }; // placeholder for run-time cycles
-
-    tryFinally(function() {
-      reified = reify(mod, name, seen[name]);
-      module = mod.callback.apply(this, reified.deps);
-      loaded = true;
-    }, function() {
-      if (!loaded) {
-        mod.state = FAILED;
-      }
-    });
-
-    var obj;
-    if (module === undefined && reified.module.exports) {
-      obj = reified.module.exports;
-    } else {
-      obj = seen[name] = module;
-    }
-
-    if (obj !== null && typeof obj === 'object' && obj['default'] === undefined) {
-      obj['default'] = obj;
-    }
-
-    return (seen[name] = obj);
-  };
-
-  function resolve(child, name) {
-    if (child.charAt(0) !== '.') { return child; }
-
-    var parts = child.split('/');
-    var nameParts = name.split('/');
-    var parentBase = nameParts.slice(0, -1);
-
-    for (var i = 0, l = parts.length; i < l; i++) {
-      var part = parts[i];
-
-      if (part === '..') { parentBase.pop(); }
-      else if (part === '.') { continue; }
-      else { parentBase.push(part); }
-    }
-
-    return parentBase.join('/');
-  }
-
-  requirejs.entries = requirejs._eak_seen = registry;
-  requirejs.clear = function(){
-    requirejs.entries = requirejs._eak_seen = registry = {};
-    seen = state = {};
-  };
-})();
-
-;define("dom-ruler/layout", 
+define("dom-ruler/layout", 
   ["dom-ruler/styles","exports"],
   function(__dependency1__, __exports__) {
     "use strict";
@@ -620,158 +474,8 @@ var define, requireModule, require, requirejs;
     __exports__.teardownTextMeasurement = teardownTextMeasurement;
     __exports__.measureText = measureText;
   });
-;define("loader", 
-  [],
-  function() {
-    "use strict";
-    var define, requireModule, require, requirejs;
-
-    (function() {
-
-      var _isArray;
-      if (!Array.isArray) {
-        _isArray = function (x) {
-          return Object.prototype.toString.call(x) === "[object Array]";
-        };
-      } else {
-        _isArray = Array.isArray;
-      }
-
-      var registry = {}, seen = {};
-      var FAILED = false;
-
-      var uuid = 0;
-
-      function tryFinally(tryable, finalizer) {
-        try {
-          return tryable();
-        } finally {
-          finalizer();
-        }
-      }
-
-
-      function Module(name, deps, callback, exports) {
-        var defaultDeps = ['require', 'exports', 'module'];
-
-        this.id       = uuid++;
-        this.name     = name;
-        this.deps     = !deps.length && callback.length ? defaultDeps : deps;
-        this.exports  = exports || { };
-        this.callback = callback;
-        this.state    = undefined;
-      }
-
-      define = function(name, deps, callback) {
-        if (!_isArray(deps)) {
-          callback = deps;
-          deps     =  [];
-        }
-
-        registry[name] = new Module(name, deps, callback);
-      };
-
-      define.amd = {};
-
-      function reify(mod, name, seen) {
-        var deps = mod.deps;
-        var length = deps.length;
-        var reified = new Array(length);
-        var dep;
-        // TODO: new Module
-        // TODO: seen refactor
-        var module = { };
-
-        for (var i = 0, l = length; i < l; i++) {
-          dep = deps[i];
-          if (dep === 'exports') {
-            module.exports = reified[i] = seen;
-          } else if (dep === 'require') {
-            reified[i] = function requireDep(dep) {
-              return require(resolve(dep, name));
-            };
-          } else if (dep === 'module') {
-            mod.exports = seen;
-            module = reified[i] = mod;
-          } else {
-            reified[i] = require(resolve(dep, name));
-          }
-        }
-
-        return {
-          deps: reified,
-          module: module
-        };
-      }
-
-      requirejs = require = requireModule = function(name) {
-        var mod = registry[name];
-        if (!mod) {
-          throw new Error('Could not find module ' + name);
-        }
-
-        if (mod.state !== FAILED &&
-            seen.hasOwnProperty(name)) {
-          return seen[name];
-        }
-
-        var reified;
-        var module;
-        var loaded = false;
-
-        seen[name] = { }; // placeholder for run-time cycles
-
-        tryFinally(function() {
-          reified = reify(mod, name, seen[name]);
-          module = mod.callback.apply(this, reified.deps);
-          loaded = true;
-        }, function() {
-          if (!loaded) {
-            mod.state = FAILED;
-          }
-        });
-
-        var obj;
-        if (module === undefined && reified.module.exports) {
-          obj = reified.module.exports;
-        } else {
-          obj = seen[name] = module;
-        }
-
-        if (obj !== null && typeof obj === 'object' && obj['default'] === undefined) {
-          obj['default'] = obj;
-        }
-
-        return (seen[name] = obj);
-      };
-
-      function resolve(child, name) {
-        if (child.charAt(0) !== '.') { return child; }
-
-        var parts = child.split('/');
-        var nameParts = name.split('/');
-        var parentBase = nameParts.slice(0, -1);
-
-        for (var i = 0, l = parts.length; i < l; i++) {
-          var part = parts[i];
-
-          if (part === '..') { parentBase.pop(); }
-          else if (part === '.') { continue; }
-          else { parentBase.push(part); }
-        }
-
-        return parentBase.join('/');
-      }
-
-      requirejs.entries = requirejs._eak_seen = registry;
-      requirejs.clear = function(){
-        requirejs.entries = requirejs._eak_seen = registry = {};
-        seen = state = {};
-      };
-    })();
-  });
-define("truncate", 
-  ["truncate/css","truncate/utils","truncate/words","dom-ruler/text","dom-ruler/utils","dom-ruler/layout","exports"],
+define("truncate-text", 
+  ["truncate-text/css","truncate-text/utils","truncate-text/words","dom-ruler/text","dom-ruler/utils","dom-ruler/layout","exports"],
   function(__dependency1__, __dependency2__, __dependency3__, __dependency4__, __dependency5__, __dependency6__, __exports__) {
     "use strict";
     var splitOnSoftWrapOpportunities = __dependency1__.splitOnSoftWrapOpportunities;
@@ -839,8 +543,8 @@ define("truncate",
     var truncate = truncate;
     __exports__.truncate = truncate;
   });
-;define("truncate/css", 
-  ["truncate/utils","exports"],
+;define("truncate-text/css", 
+  ["truncate-text/utils","exports"],
   function(__dependency1__, __exports__) {
     "use strict";
     // LineBreak-7.0.0.txt
@@ -895,7 +599,7 @@ define("truncate",
 
     __exports__.splitOnSoftWrapOpportunities = splitOnSoftWrapOpportunities;
   });
-;define("truncate/utils", 
+;define("truncate-text/utils", 
   ["exports"],
   function(__exports__) {
     "use strict";
@@ -909,8 +613,8 @@ define("truncate",
 
     __exports__.trim = trim;
   });
-;define("truncate/words", 
-  ["truncate/css","dom-ruler/text","dom-ruler/layout","exports"],
+;define("truncate-text/words", 
+  ["truncate-text/css","dom-ruler/text","dom-ruler/layout","exports"],
   function(__dependency1__, __dependency2__, __dependency3__, __exports__) {
     "use strict";
     var splitOnSoftWrapOpportunities = __dependency1__.splitOnSoftWrapOpportunities;
